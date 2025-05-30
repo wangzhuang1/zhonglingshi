9da620a7a18839eee960b98efec36be71000c30b
wangzhuang <wangzhuang1027@126.com>
2025-05-30 17:15:14 +0800

    增加类目

--- src/json/index.ts
+++ src/json/index.ts
@@ -27,9 +27,45 @@ const y63 = {
     price: [718]
 }
 
+const heibaoshi = {
+    title: '6级黑宝石',
+    time: ['2025/5/30'],
+    price: [39]
+}
+
+const xinghuishi = {
+    title: '6级星辉石',
+    time: ['2025/5/30'],
+    price: [286]
+}
+
+const gaolian = {
+    title: '高级连击',
+    time: ['2025/5/30'],
+    price: [392]
+}
+
+const gaobi = {
+    title: '高级必杀',
+    time: ['2025/5/30'],
+    price: [279]
+}
+
+const gaotou = {
+    title: '高级偷袭',
+    time: ['2025/5/30'],
+    price: [386]
+}
+
+const gaoshen = {
+    title: '高级神佑',
+    time: ['2025/5/30'],
+    price: [95]
+}
+
 
 const btnArr = [
-    jianbu, xueqi, huichun, ruibu, y63
+    jianbu, xueqi, huichun, ruibu, y63, heibaoshi, xinghuishi, gaolian, gaobi, gaotou, gaoshen
 ]
 
 export default btnArr;
