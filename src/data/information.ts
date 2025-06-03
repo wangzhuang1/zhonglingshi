475df8107637c3e3274a165ebce2b72b8cb0a45d
wangzhuang <wangzhuang1027@126.com>
2025-06-03 12:34:42 +0800

    更新数据

--- src/data/information.ts
+++ src/data/information.ts
@@ -1,8 +1,10 @@
 const information = [
     {
-        time: '2025-6-1：',
-        text: '相关更新'
+        time: '2025-6-3：',
+        text: '维护更新，五行大师增加186临时符，售价15万；宝图一次可以取5张；'
     }
 ]
+
+information.reverse()
 
 export default information;
--- src/data/price.ts
+++ src/data/price.ts
@@ -1,79 +1,79 @@
-const time = ['2025/5/30', '2025/5/31', '2025/6/1', '2025/6/2'];
+const time = ['2025/5/30', '2025/5/31', '2025/6/1', '2025/6/2', '2025/6/2'];
 const jianbu = {
     title: '健步如飞',
     time,
-    price: [1160, 1218, 1200, 1199]
+    price: [1160, 1218, 1200, 1199, 1200]
 }
 
 const xueqi = {
     title: '血气方刚',
     time,
-    price: [236, 239, 239, 230]
+    price: [236, 239, 239, 230, 246]
 }
 const huichun = {
     title: '回春之术',
     time,
-    price: [108, 107, 107, 105]
+    price: [108, 107, 107, 105, 106]
 }
 
 const ruibu = {
     title: '锐不可当',
     time,
-    price: [399, 395, 395, 395]
+    price: [399, 395, 395, 395, 395]
 }
 
 const y63 = {
     title: '163',
     time,
-    price: [718, 739, 719, 719]
+    price: [718, 739, 719, 719, 719]
 }
 
 const heibaoshi = {
     title: '6级黑宝石',
     time,
-    price: [39, 38, 39, 38]
+    price: [39, 38, 39, 38, 38]
 }
 
 const xinghuishi = {
     title: '6级星辉石',
     time,
-    price: [286, 288, 290, 291]
+    price: [286, 288, 290, 291, 292]
 }
 
 const gaolian = {
     title: '高级连击',
     time,
-    price: [392, 390, 386, 368]
+    price: [392, 390, 386, 368, 379]
 }
 
 const gaobi = {
     title: '高级必杀',
     time,
-    price: [379, 373, 377, 376]
+    price: [379, 373, 377, 376, 379]
 }
 
 const gaotou = {
     title: '高级偷袭',
     time,
-    price: [386, 380, 380, 379]
+    price: [386, 380, 380, 379, 380]
 }
 
 const gaoshen = {
     title: '高级神佑',
     time,
-    price: [95, 93, 105, 105]
+    price: [95, 93, 105, 105, 106]
 }
 
 const MHB = {
     title: '梦幻币',
     time,
-    price: [231, 230, 228, 228]
+    price: [231, 230, 228, 228, 229]
 }
 
 const zhanpo = {
     title: '战魄',
     time,
-    price: [232, 231, 229, 232]
+    price: [232, 231, 229, 232, 231]
 }
 
 
