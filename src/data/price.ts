120d3032f49f75708c13a997d749565c8affe59a
wangzhuang <wangzhuang1027@126.com>
2025-06-06 16:28:22 +0800

    更新书记

--- src/data/price.ts
+++ src/data/price.ts
@@ -1,92 +1,140 @@
-const time = ['2025/5/30', '2025/5/31', '2025/6/1', '2025/6/2', '2025/6/3', '2025/6/4'];
+const time = [
+    '2025/5/30', '2025/5/31', '2025/6/1', '2025/6/2', '2025/6/3',
+    '2025/6/4', '2025/6/5', '2025/6/6',
+];
 const jianbu = {
     title: '健步如飞',
     time,
-    price: [1160, 1218, 1200, 1199, 1200, 1150]
+    price: [
+        1160, 1218, 1200, 1199, 1200,
+        1150, 1150, 1198
+    ]
 }
 
 const xueqi = {
     title: '血气方刚',
     time,
-    price: [236, 239, 239, 230, 246, 246]
+    price: [
+        236, 239, 239, 230, 246,
+        246, 238, 228
+    ]
 }
 const huichun = {
     title: '回春之术',
     time,
-    price: [108, 107, 107, 105, 106, 106]
+    price: [
+        108, 107, 107, 105, 106,
+        106, 105, 105
+    ]
 }
 
 const ruibu = {
     title: '锐不可当',
     time,
-    price: [399, 395, 395, 395, 395, 394]
+    price: [
+        399, 395, 395, 395, 395,
+        394, 394, 392
+    ]
 }
 
 const y63 = {
     title: '163',
     time,
-    price: [718, 739, 719, 719, 719, 718]
+    price: [
+        718, 739, 719, 719, 719,
+        718, 716, 718
+    ]
 }
 
 const heibaoshi = {
     title: '6级黑宝石',
     time,
-    price: [39, 38, 39, 38, 38, 37]
+    price: [
+        39, 38, 39, 38, 38,
+        37, 38, 37
+    ]
 }
 
 const xinghuishi = {
     title: '6级星辉石',
     time,
-    price: [286, 288, 290, 291, 292, 290]
+    price: [
+        286, 288, 290, 291, 292,
+        290, 285, 283
+    ]
 }
 
 const gaolian = {
     title: '高级连击',
     time,
-    price: [392, 390, 386, 368, 379, 369]
+    price: [
+        392, 390, 386, 368, 379,
+        369, 370, 369
+    ]
 }
 
 const gaobi = {
     title: '高级必杀',
     time,
-    price: [379, 373, 377, 376, 379, 369]
+    price: [
+        379, 373, 377, 376, 379,
+        369, 370, 367
+    ]
 }
 
 const gaotou = {
     title: '高级偷袭',
     time,
-    price: [386, 380, 380, 379, 380, 379]
+    price: [
+        386, 380, 380, 379, 380,
+        379, 372, 373
+    ]
 }
 
 const gaoshen = {
     title: '高级神佑',
     time,
-    price: [95, 93, 105, 105, 106, 97]
-}
-
-
-const MHB = {
-    title: '梦幻币',
-    time,
-    price: [231, 230, 228, 228, 229, 226]
+    price: [
+        95, 93, 105, 105, 106,
+        97, 97, 100
+    ]
 }
 
 const zhanpo = {
     title: '战魄',
     time,
-    price: [232, 231, 229, 232, 231, 229]
+    price: [
+        232, 231, 229, 232, 231,
+        229, 233, 233
+    ]
 }
+
+const MHB = {
+    title: '梦幻币',
+    time,
+    price: [
+        231, 230, 228, 228, 229,
+        226, 226, 228
+    ]
+}
+
+
 
 const fumo80 = {
     title: '80附魔',
     time,
-    price: [930, 930, 920, 910, 910, 910]
+    price: [
+        930, 930, 920, 910, 910,
+        910, 910, 910
+    ]
 }
 
 
 
 const btnArr = [
-    jianbu, xueqi, huichun, ruibu, y63, heibaoshi, xinghuishi, gaolian, gaobi, gaotou, gaoshen, MHB, zhanpo, fumo80
+    jianbu, xueqi, huichun, ruibu, y63,
+    heibaoshi, xinghuishi, gaolian, gaobi, gaotou,
+    gaoshen, zhanpo, MHB, fumo80
 ]
 
 export default btnArr;
